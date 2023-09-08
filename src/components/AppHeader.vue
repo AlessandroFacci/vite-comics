67c87f10a0bee7b7fa37201f9f8e496f591dafd6
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-08 16:17:56 +0200

    AppHeader: add links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,72 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      links: [
+        {
+          text: "CHARACTERS",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "COMICS",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "MOVIES",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "TV",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "GAMES",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "COLLECTIBLES",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "VIDEOS",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "FANS",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "NEWS",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "SHOP",
+          url: "#",
+          current: "false",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
-  <header></header>
+  <header>
+    <ul>
+      <li v-for="link in links">
+        <a :href="link.url"> {{ link.text }}</a>
+      </li>
+    </ul>
+  </header>
 </template>
 
 <style lang="scss" scoped></style>
