575e799c220053fe3e4fbc19076610ca9db332c1
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-08 16:36:32 +0200

    App: create and add AppFooter

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
 import AppContent from "./components/AppContent.vue";
+import AppFooter from "./components/AppFooter.vue";
 
 export default {
   data() {
     return {};
   },
-  components: { AppHeader, AppContent },
+  components: { AppHeader, AppContent, AppFooter },
 };
 </script>
 
 <template>
   <AppHeader></AppHeader>
   <AppContent></AppContent>
+  <AppFooter></AppFooter>
 </template>
 
 <style lang="scss">
