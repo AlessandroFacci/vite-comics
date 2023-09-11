37ea89fa31f5bec875cac775153e0478881f5130
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-11 16:37:04 +0200

    AppContent: add card

---
+++ src/components/CardProduct.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  props: {
+    movie: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <img :src="movie.thumb" alt="img" />
+    <h3>{{ movie.series }}</h3>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  width: 16%;
+  gap: 3%;
+  img {
+    width: 100%;
+  }
+  h3 {
+    color: white;
+    margin-top: 1rem;
+  }
+}
+</style>
